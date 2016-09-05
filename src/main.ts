5a0a40dcb674b81b69b584cc89d1e61a61244023
Oliver Joseph Ash <oliverjash@gmail.com>
2016-09-05 22:55:49 +0100

    Render result

--- src/main.ts
+++ src/main.ts
@@ -29,53 +29,94 @@ const createStore = <Action, State>(reducer: ReducerFn<Action, State>, initialSt
 
 
 type Task<Success, Error> = () => Promise<Success | Error>
-const performTask = <Success, Error>(success: Action, error: Action, task: Task<Success, Error>): Cmd<Action> => {
-    return () => task().then(() => success, () => error)
+type CreateActionFn<Msg, Action> = (msg: Msg) => Action;
+const performTask = <SuccessAction, ErrorAction, Success, Error>(
+    createSuccessAction: CreateActionFn<Success, SuccessAction>,
+    createErrorAction: CreateActionFn<Error, ErrorAction>,
+    task: Task<Success, Error>): Cmd<Action> => {
+    return () => task().then(createSuccessAction, createErrorAction)
 }
 
-const httpGet = (url: string): Task<string, string> => {
-    return () => (
-        fetch(url).then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                return 'bad response';
-            }
-        })
+const httpGet = (url: string): Task<string, string> => (
+    () => (
+        fetch(url)
+            .then(response => (
+                response.ok
+                    ? response.json().then(json => JSON.stringify(json, null, '\t'))
+                    : Promise.resolve('bad response')
+            ))
     )
-}
+);
 
 // End lib
 
-const getRandomGif = <Action>(topic: string): Cmd<Action> => {
-    const url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + topic;
-    return performTask(
-        { type: ActionTypes.FetchSuccess },
-        { type: ActionTypes.FetchError },
-        httpGet(url)
-    )
+const create = <T>(t: T): T => t;
+
+export interface Success<T> {
+    success: true;
+    value: T;
 }
 
-type State = 'not started' | 'pending' | 'success' | 'error';
+export interface Failure {
+    success: false;
+    reason: string;
+}
+
+type Result<T> = Success<T> | Failure;
+
 enum ActionTypes { Fetch, FetchSuccess, FetchError };
-type Action = { type: ActionTypes.Fetch } | { type: ActionTypes.FetchSuccess } | { type: ActionTypes.FetchError }
+type FetchAction = { type: ActionTypes.Fetch };
+type FetchSuccessAction = { type: ActionTypes.FetchSuccess, result: Result<string> };
+const createFetchSuccessAction = (result: string): FetchSuccessAction => ({
+    type: ActionTypes.FetchSuccess,
+    result: create<Success<string>>({ success: true, value: result })
+});
+type FetchErrorAction = { type: ActionTypes.FetchError, result: Result<string> };
+const createFetchErrorAction = (result: string): FetchErrorAction => ({
+    type: ActionTypes.FetchError,
+    result: create<Failure>({ success: false, reason: result })
+});
+type Action = FetchAction | FetchSuccessAction | FetchErrorAction;
+
+const getRandomGif = <Action>(topic: string): Cmd<Action> => {
+    const url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + topic;
+    return performTask(createFetchSuccessAction, createFetchErrorAction, httpGet(url))
+}
+
+type State = {
+    status: 'not started' | 'pending' | 'success' | 'error',
+    result: Option<Result<string>>
+};
 const reducer: ReducerFn<Action, State> = (action: Action, state: State): [State, Option<Cmd<Action>>] => {
     switch (action.type) {
         case ActionTypes.Fetch:
-            return ['pending', new Some(getRandomGif('food'))];
+            return [{ status: 'pending', result: None }, new Some(getRandomGif('food'))];
         case ActionTypes.FetchSuccess:
-            return ['success', None]
+            return [{ status: 'success', result: new Some(action.result) }, None]
         case ActionTypes.FetchError:
-            return ['error', None]
-        default:
-            return [state, None];
+            return [{ status: 'error', result: new Some(action.result) }, None]
     }
 }
 
-const initialState: State = 'not started';
+const initialState: State = {
+    status: 'not started',
+    result: None
+};
 const store = createStore(reducer, initialState);
 
 const rootEl = document.getElementById('root');
-store.subscribe(() => { if (rootEl) rootEl.innerHTML = store.getState() });
+store.subscribe(() => {
+    const state = store.getState();
+    if (rootEl) rootEl.innerHTML = `
+<pre>
+Status: ${state.status}
+Result success: ${state.result
+    .map(result => result.success)
+    .getOrElse(() => false)}
+Result success value/failure reason: ${state.result
+    .map(result => result.success ? result.value : result.reason)
+    .getOrElse(() => '')}
+</pre>`;
+});
 
 store.dispatch({ type: ActionTypes.Fetch });
