4148d7468515ea5b206b4caa0ac79901511372dd
Oliver Joseph Ash <oliverjash@gmail.com>
2016-09-07 15:07:50 +0100

    WIP

--- src/enhancer.ts
+++ src/enhancer.ts
@@ -1,13 +1,28 @@
-import {Option} from 'monapt';
+import {flatten as flattenOptions, Some, Option} from 'monapt';
 import {StoreEnhancer, StoreCreator, Reducer, Action, Store} from 'redux';
+import {flattenDeep as flattenDeepArrays} from 'lodash';
 
-export type Cmd<A extends Action> = () => Promise<A>;
-type EnhancedReducer<A extends Action, State> = (state: State, action: A) => [State, Option<Cmd<A>>];
+// http://guide.elm-lang.org/effect_managers/batching.html
+// http://package.elm-lang.org/packages/elm-lang/core/latest/Platform-Cmd#batch
+// https://github.com/redux-loop/redux-loop/pull/69/files#diff-cb03f5868274bee764df3e04d233383dR35
+// https://github.com/evancz/elm-architecture-tutorial/blob/2936ab64702293a0ce41ab4ed9c2344f7f1a4de6/nesting/4-gif-list.elm
 
-type EnhancedStoreCreator<A extends Action, S> = (reducer: EnhancedReducer<A, S>, initialState: S, enhancer?: StoreEnhancer<S>) => Store<S>
-type Enhancer<A extends Action, S> = (originalStoreCreator: StoreCreator) => EnhancedStoreCreator<A, S>;
+export type Cmd<A extends Action> = () => Promise<A[]>;
+export const Cmd = {
+    batch: (cmds: Cmd<Action>[]): Cmd<Action> => (
+        () => Promise.all(cmds.map(fn => fn())).then(flattenDeepArrays)
+    )
+}
 
-const liftReducer = <A extends Action, S>(reducer: EnhancedReducer<A, S>, callback: (cmd: Cmd<A>) => void): Reducer<S> => {
+type EnhancedReducer<State> = <A extends Action>(state: State, action: A) => [State, Option<Cmd<A>>];
+type EnhancedReducersMapObject = {
+    [key: string]: EnhancedReducer<any>;
+}
+
+type EnhancedStoreCreator<S> = <A extends Action>(reducer: EnhancedReducer<S>, initialState: S, enhancer?: StoreEnhancer<S>) => Store<S>
+type Enhancer<S> = (originalStoreCreator: StoreCreator) => EnhancedStoreCreator<S>;
+
+const liftReducer = <A extends Action, S>(reducer: EnhancedReducer<S>, callback: (cmd: Cmd<A>) => void): Reducer<S> => {
     return (state: S, action: A) => {
         const [newState, maybeCommand] = reducer(state, action);
         maybeCommand.foreach(callback)
@@ -23,9 +38,9 @@ const createSubject = <T>() => {
     return { onNext, subscribe }
 }
 
-export const install = <A extends Action, S>(): Enhancer<A, S> => {
-    return (originalCreateStore): EnhancedStoreCreator<A, S> => {
-        return (reducer, initialState, enhancer) => {
+export const install = <S>(): Enhancer<S> => {
+    return (originalCreateStore): EnhancedStoreCreator<S> => {
+        return <A extends Action>(reducer: EnhancedReducer<S>, initialState: S, enhancer?: StoreEnhancer<S>) => {
             // This subject represents a stream of cmds coming from
             // the reducer
             const subject = createSubject<Cmd<A>>();
@@ -33,29 +48,56 @@ export const install = <A extends Action, S>(): Enhancer<A, S> => {
             const store = originalCreateStore(liftedReducer, initialState, enhancer)
             // Close the loop by running the command and dispatching to the
             // store
-            subject.subscribe(command => command().then(store.dispatch));
+            subject.subscribe(cmd => cmd().then(actions => actions.forEach(store.dispatch)));
 
             return store;
         }
     }
 }
 
+// TODO: ErrorMsg is really thrown, not typed!
 type Task<SuccessMsg, ErrorMsg> = () => Promise<SuccessMsg | ErrorMsg>
 type CreateActionFn<Msg, Action> = (msg: Msg) => Action;
 export const performTask = <SuccessAction extends Action, ErrorAction extends Action, SuccessMsg, ErrorMsg>(
     createSuccessAction: CreateActionFn<SuccessMsg, SuccessAction>,
     createErrorAction: CreateActionFn<ErrorMsg, ErrorAction>,
-    task: Task<SuccessMsg, ErrorMsg>): Cmd<SuccessAction | ErrorAction> => {
+	task: Task<SuccessMsg, ErrorMsg>
+): Cmd<SuccessAction | ErrorAction> => {
     return () => task().then(createSuccessAction, createErrorAction)
 }
 
-export const httpGet = (url: string): Task<string, string> => (
+export const httpGet = <Success>(decoder: (x: any) => Success, url: string, fetchOptions?: RequestInit): Task<Success, Error> => (
     () => (
-        fetch(url)
-            .then(response => (
-                response.ok
-                    ? response.json().then(json => JSON.stringify(json, null, '\t'))
-                    : Promise.resolve('bad response')
-            ))
+        fetch(url, fetchOptions)
+            .then(response => response.json())
+			.then(decoder)
     )
 );
+
+// https://github.com/redux-loop/redux-loop/blob/c708d98a9960d9efe3accc3acbc8f86c940941fa/modules/combineReducers.js
+export function combineReducers(reducerMap: EnhancedReducersMapObject): EnhancedReducer<any> {
+    return function finalReducer(state: any, action: Action): [any, Option<Cmd<Action>>] {
+        type Accumulator = {
+            state: any,
+            commands: Option<Cmd<Action>>[]
+            hasChanged: boolean
+        };
+        const model = Object.keys(reducerMap).reduce<Accumulator>((acc, key) => {
+            const reducer = reducerMap[key];
+            const previousStateForKey = state[key];
+            const nextResultForKey = reducer(previousStateForKey, action);
+            const [nextStateForKey, maybeCommand] = nextResultForKey;
+
+            acc.hasChanged = acc.hasChanged || nextStateForKey !== previousStateForKey;
+            acc.state[key] = nextStateForKey;
+            acc.commands.push(maybeCommand);
+            return acc;
+        }, {
+            state: {},
+            commands: [],
+            hasChanged: false
+        });
+
+        return [model.state, new Some(Cmd.batch(flattenOptions(model.commands)))];
+    };
+}
--- src/example.ts
+++ src/example.ts
@@ -1,8 +1,8 @@
 import {Option,Some,None} from 'monapt';
-import {install, Cmd, performTask, httpGet} from './enhancer';
+import {combineReducers, install, Cmd, performTask, httpGet} from './enhancer';
 import {createStore} from 'redux';
 
-const enhancedCreateStore = install<Action, State>()(createStore);
+const enhancedCreateStore = install<State>()(createStore);
 
 const create = <T>(t: T): T => t;
 
@@ -18,7 +18,7 @@ export interface Failure {
 
 type Result<T> = Success<T> | Failure;
 
-enum ActionTypes { Fetch, FetchSuccess, FetchError };
+enum ActionTypes { Fetch, FetchSuccess, FetchError, Test };
 type FetchAction = { type: ActionTypes.Fetch };
 type FetchSuccessAction = { type: ActionTypes.FetchSuccess, result: Result<string> };
 const createFetchSuccessAction = (result: string): FetchSuccessAction => ({
@@ -30,47 +30,64 @@ const createFetchErrorAction = (result: string): FetchErrorAction => ({
     type: ActionTypes.FetchError,
     result: create<Failure>({ success: false, reason: result })
 });
-type Action = FetchAction | FetchSuccessAction | FetchErrorAction;
+type Action = FetchAction | FetchSuccessAction | FetchErrorAction | { type: ActionTypes.Test };
 
+const decodeGifUrl = (response: any): string => response.data.image_url;
 const getRandomGif = (topic: string): Cmd<FetchSuccessAction | FetchErrorAction> => {
     const url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + topic;
-    return performTask(createFetchSuccessAction, createFetchErrorAction, httpGet(url))
+    return performTask(
+        createFetchSuccessAction,
+        createFetchErrorAction,
+        httpGet(decodeGifUrl, url)
+    )
 }
 
-type State = {
+type MainState = {
     status: 'not started' | 'pending' | 'success' | 'error',
     result: Option<Result<string>>
+}
+type State = {
+    main: MainState
 };
-const reducer = (state: State, action: Action): [State, Option<Cmd<Action>>] => {
+const reducer = (state: MainState, action: Action): [MainState, Option<Cmd<Action>>] => {
     switch (action.type) {
         case ActionTypes.Fetch:
             return [{ status: 'pending', result: None }, new Some(getRandomGif('food'))];
         case ActionTypes.FetchSuccess:
-            return [{ status: 'success', result: new Some(action.result) }, None]
+            return [{ status: 'success', result: new Some(action.result) }, new Some(
+                Cmd.batch([Cmd.batch([() => Promise.resolve({ type: ActionTypes.Test })])])
+            )]
         case ActionTypes.FetchError:
             return [{ status: 'error', result: new Some(action.result) }, None]
+        // withDefault
+        case ActionTypes.Test:
+            console.log('it worked')
+            return [state, None]
         default:
             return [state, None];
     }
 }
 
 const initialState: State = {
-    status: 'not started',
-    result: None
+    main: {
+        status: 'not started',
+        result: None
+    }
 };
-const store = enhancedCreateStore(reducer, initialState);
+
+const store = enhancedCreateStore(combineReducers({ main: reducer }), initialState);
 
 const rootEl = document.getElementById('root');
 store.subscribe(() => {
     const state = store.getState();
     if (rootEl) rootEl.innerHTML = `
 <pre>
-Status: ${state.status}
-Result success: ${state.result
+Status: ${state.main.status}
+Result success: ${state.main.result
     .map(result => result.success)
     .getOrElse(() => false)}
-Result success value/failure reason: ${state.result
-    .map(result => result.success ? result.value : result.reason)
+Result success value/failure reason: ${state.main.result
+    .map(result => result.success ? JSON.stringify(result.value, null, '\t') : result.reason)
     .getOrElse(() => '')}
 </pre>`;
 });
