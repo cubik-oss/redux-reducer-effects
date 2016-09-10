e966e2ce97aca977f05545e4ad9c7ef46f81ea61
Oliver Joseph Ash <oliverjash@gmail.com>
2016-09-10 21:21:43 +0100

    Tidy up

--- src/example.ts
+++ src/example.ts
@@ -2,55 +2,45 @@ import {Option,Some,None} from 'monapt';
 import enhance, {combineReducers, TaskRunner} from './enhancer';
 import {createStore} from 'redux';
 
-// createStore(reducer, enhance)
 const enhancedCreateStore = enhance(createStore);
 
 const create = <T>(t: T): T => t;
 
 export type Success<A> = { success: true; value: A; }
 export type Error<X> = { success: false; value: X }
-
 type Result<X, A> = Error<X> | Success<A>;
 
-enum ActionTypes { Fetch, FetchSuccess, FetchError, Test, RunTask, RunTaskDone };
-type RunTaskDoneAction = { type: ActionTypes.RunTaskDone };
-const createRunTaskDoneAction = (): RunTaskDoneAction => (
-    { type: ActionTypes.RunTaskDone }
-)
+enum ActionTypes { Fetch, FetchSuccess, FetchError };
 type FetchAction = { type: ActionTypes.Fetch };
-type FetchSuccessAction = { type: ActionTypes.FetchSuccess, result: Result<string, string> };
+type FetchSuccessAction = { type: ActionTypes.FetchSuccess, result: Success<string> };
 const createFetchSuccessAction = (result: Success<string>): FetchSuccessAction => ({
     type: ActionTypes.FetchSuccess,
     result
 });
-type FetchErrorAction = { type: ActionTypes.FetchError, result: Result<string, string> };
+type FetchErrorAction = { type: ActionTypes.FetchError, result: Error<string> };
 const createFetchErrorAction = (result: Error<string>): FetchErrorAction => ({
     type: ActionTypes.FetchError,
     result
 });
-type FetchResponseAction = FetchSuccessAction | FetchErrorAction;
-type Action = FetchAction | FetchResponseAction | { type: ActionTypes.Test } | { type: ActionTypes.RunTask } | RunTaskDoneAction;
+type Action = FetchAction | FetchSuccessAction | FetchErrorAction;
 
 type TaskError<X> = { success: false, value: X }
 type TaskSuccess<A> = { success: true, value: A }
 
-export const createTaskError = <X>(x: X): TaskError<X> => ({ success: false, value: x })
-export const createTaskSuccess = <A>(a: A): TaskSuccess<A> => ({ success: true, value: a })
+export const createError = <X>(x: X): Error<X> => ({ success: false, value: x })
+export const createSuccess = <A>(a: A): Success<A> => ({ success: true, value: a })
 
-enum TaskTypes { GetRandomGif, RunTask }
 type GetRandomGifTask = {
-    type: TaskTypes.GetRandomGif,
+    type: 'GetRandomGif',
     topic: string,
     onFail: (x: TaskError<string>) => FetchErrorAction,
     onSuccess: (a: TaskSuccess<string>) => FetchSuccessAction,
 }
-type RunTaskTask = { type: TaskTypes.RunTask }
-type Task = GetRandomGifTask | RunTaskTask
+type Task = GetRandomGifTask;
 
 type MainState = {
     status: 'not started' | 'pending' | 'success' | 'error',
-    result: Option<Result<string, string>>,
-    constantRan: boolean
+    result: Option<Result<string, string>>
 }
 type State = {
     main: MainState
@@ -61,7 +51,7 @@ const reducer = (state: MainState, action: Action): [MainState, Option<Task[]>] 
         case ActionTypes.Fetch:
             return [patch(state, { status: 'pending', result: None }), new Some([
                 create<GetRandomGifTask>({
-                    type: TaskTypes.GetRandomGif,
+                    type: 'GetRandomGif',
                     topic: 'food',
                     onFail: createFetchErrorAction,
                     onSuccess: createFetchSuccessAction,
@@ -71,13 +61,6 @@ const reducer = (state: MainState, action: Action): [MainState, Option<Task[]>] 
             return [patch(state, { status: 'success', result: new Some(action.result) }), None]
         case ActionTypes.FetchError:
             return [patch(state, { status: 'error', result: new Some(action.result) }), None]
-
-        case ActionTypes.RunTask:
-            return [state, new Some([
-                create<RunTaskTask>({ type: TaskTypes.RunTask })
-            ])]
-        case ActionTypes.RunTaskDone:
-            return [patch(state, { constantRan: true }), None]
         default:
             return [state, None];
     }
@@ -86,37 +69,29 @@ const reducer = (state: MainState, action: Action): [MainState, Option<Task[]>] 
 const initialState: State = {
     main: {
         status: 'not started',
-        result: None,
-        constantRan: false,
+        result: None
     }
 };
 
-enum EffectTypes { Fetch }
-type FetchEffect<Value> = {
-    type: EffectTypes.Fetch,
-    url: string,
-    fetchOptions?: RequestInit,
-    decoder: (x: any) => Value
-}
-
 const createGifUrl = (topic: string): string => `https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=${topic}`;
 const decodeGifUrl = (response: any): string => response.data.image_url;
+// Callbacks must be on Task because otherwise we don't know their type
+// Is result of Task compaitable with callback? We only know this if
+// they are coupled
 const myTaskRunner: TaskRunner<Action> = <X, A>(task: Task): Promise<Action> => {
     switch (task.type) {
-        case TaskTypes.GetRandomGif:
+        case 'GetRandomGif':
             const url = createGifUrl(task.topic);
             return fetch(url)
                 .then(response => response.json())
                 .then(decodeGifUrl)
-                .then(createTaskSuccess)
-                .catch(createTaskError)
+                .then(createSuccess)
+                .catch(createError)
                 .then(result => (
                     result.success
                         ? task.onSuccess(result)
                         : task.onFail(result)
                 ))
-        case TaskTypes.RunTask:
-            return Promise.resolve(createRunTaskDoneAction())
     }
 }
 
@@ -134,11 +109,7 @@ Result success: ${state.main.result
 Result success value/failure reason: ${state.main.result
     .map(result => result.success ? JSON.stringify(result.value, null, '\t') : result.value)
     .getOrElse(() => '')}
-Constant ran: ${state.main.constantRan}
 </pre>`;
 });
 
 store.dispatch({ type: ActionTypes.Fetch });
-setTimeout(() => {
-    store.dispatch({ type: ActionTypes.RunTask });
-}, 1000)
