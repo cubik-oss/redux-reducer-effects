a191a411a3679673c702ec660fbcca25bef25bf5
Tim Ruffles <timruffles@gmail.com>
2016-09-16 09:57:53 +0100

    chore: reindent

--- src/test.ts
+++ src/test.ts
@@ -4,57 +4,56 @@ import { assert } from "chai";
 type Action = { type : string};
 type Task = { task: string };
 type State = {
-  counter: number,
+    counter: number,
 };
 
 describe("redux-reducer-effects", function() {
 
-  describe("compose reducers", function() {
+    describe("compose reducers", function() {
 
-    it("keeps all state changes", function() {
-      const combined = composeReducers(increment, increment, increment);
+        it("keeps all state changes", function() {
+            const combined = composeReducers(increment, increment, increment);
 
-      const [state, _ts] = <any>(combined({ counter: 0 }, { type: "init" }));
-      assert(state, "expected final state");
-      assert.equal(state.counter, 3);
+            const [state, _ts] = <any>(combined({ counter: 0 }, { type: "init" }));
+            assert(state, "expected final state");
+            assert.equal(state.counter, 3);
 
-      function increment(s: State, action: Action) {
-        return {
-          counter: s.counter + 1,
-        }
-      }
+            function increment(s: State, action: Action) {
+                return {
+                    counter: s.counter + 1,
+                }
+            }
+        })
+
+        it("combines tasks, from both states with and without new tasks", function() {
+            const combined = composeReducers(increment, incrementWithTask, increment, incrementWithTask);
+            const stubTask = () => ({ task: "do thing" });
+
+            const [state, tasks] = <any>combined({ counter: 0 }, { type: "init" });
+
+            assert(state, "expected final state");
+            assert.equal(state.counter, 4);
+            assert.deepEqual(tasks, [stubTask(), stubTask()]);
+
+            function increment(s: State, action: Action) {
+                return {
+                    counter: s.counter + 1,
+                }
+            }
+
+            function incrementWithTask(s: State, action: Action): Result<State, Task> {
+                return [{
+                    counter: s.counter + 1,
+                }, stubTask()]
+            }
+        })
+
+            
     })
 
-    it("combines tasks, from both states with and without new tasks", function() {
-      const combined = composeReducers(increment, incrementWithTask, increment, incrementWithTask);
-      const stubTask = () => ({ task: "do thing" });
+    describe("combine reducers", function() {
 
-      const [state, tasks] = <any>combined({ counter: 0 }, { type: "init" });
-
-      assert(state, "expected final state");
-      assert.equal(state.counter, 4);
-      assert.deepEqual(tasks, [stubTask(), stubTask()]);
-
-      function increment(s: State, action: Action) {
-        return {
-          counter: s.counter + 1,
-        }
-      }
-
-      function incrementWithTask(s: State, action: Action): Result<State, Task> {
-        return [{
-          counter: s.counter + 1,
-        }, stubTask()]
-      }
+            
     })
-
-      
-  })
-
-  describe("combine reducers", function() {
-
-      
-  })
-    
 })
 
